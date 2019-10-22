8482d7c812368b1d53f74a8bc975e2a92d724bec
Alper Kayabaşı <alperkayabasi@teknasyon.com>
2019-10-22 10:32:53 +0300

    initial

---
+++ VerifyKit.framework/Headers/VerifyKit.h
@@ -0,0 +1,19 @@
+//
+//  VerifyKit.h
+//  VerifyKit
+//
+//  Created by Alper Kayabaşı on 21.10.2019.
+//  Copyright © 2019 Teknasyon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VerifyKit.
+FOUNDATION_EXPORT double VerifyKitVersionNumber;
+
+//! Project version string for VerifyKit.
+FOUNDATION_EXPORT const unsigned char VerifyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VerifyKit/PublicHeader.h>
+
+
